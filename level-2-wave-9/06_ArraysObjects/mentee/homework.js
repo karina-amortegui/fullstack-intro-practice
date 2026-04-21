f70a541756acbc135c92fa4ef099bcb51873dce1
Karina Amortegui <kamortegui70@gmail.com>
2026-04-21 14:55:02 -0700

    Add: hw 6 arrays and objects content

---
+++ level-2-wave-9/06_ArraysObjects/mentee/homework.js
@@ -0,0 +1,44 @@
+// HOMEWORK — SIMPLE CONTACT CARD
+// -----------------------------------------------
+
+// STEP 1: Create an array called contacts.
+//         It should contain at least 2 objects.
+
+// STEP 2: Each contact object needs:
+//         name (string)
+//         phone (string or number)
+//         isFavorite (boolean)
+
+// STEP 3: Log the total number of contacts.
+
+// STEP 4: Log the name of the first contact.
+
+// STEP 5: Update one contact’s isFavorite value.
+
+// STEP 6: Add (push) a new contact object.
+
+// STEP 7: Log the updated contacts array.
+
+const contacts = ["Nat", "Navid"];
+
+const nat = {
+    name: "Nat",
+    phone: "360-889-8976",
+    isFavorite: false,
+};
+
+const navid = {
+    name: "Navid",
+    phone: "360-334-4675",
+    isFavorite: true,
+};
+
+console.log("Total Contacts:", contacts.length);
+
+console.log("First Contact Name:", contacts[0]);
+
+nat.isFavorite = true;
+
+contacts[2] = "Neftaly";
+
+console.log(contacts);
